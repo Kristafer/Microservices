f545be038c7f20bc21e57fe0a1203b38b7665e61
Kirill Piskarev <kristafer177@gmail.com>
2022-05-17 22:26:52 +0200

    Add Init project

---
+++ PlatformService/Models/Platform.cs
@@ -0,0 +1,8 @@
+namespace PlatformService.Models{
+    public class Platform{
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Publish { get; set; }
+        public string Cost { get; set; }
+    }
+}
